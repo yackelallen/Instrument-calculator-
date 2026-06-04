6ac4099c06aeec38275c9935d706e87edb5bb8e2
yackelallen <yackelcole@gmail.com>
2026-06-04 09:13:11 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "yit-cache-v3-0-10";
+const CACHE_NAME = "yit-cache-v3-0-11";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
   "./manifest.json",
   "./icon-512.PNG",
   "./version.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
@@ -16,7 +16,31 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
+  const requestUrl = new URL(event.request.url);
+
+  if (
+    requestUrl.pathname.endsWith("/") ||
+    requestUrl.pathname.endsWith("/index.html")
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
